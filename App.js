dfacd9848ad6615eef13b5685231d4c568900cd3
zahara120 <zaharaa120@gmail.com>
2024-09-02 23:09:06 +0700

    setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IT'S JUST A BEGINNING</Text>
+      <Text>IT'S JUST A BEGINNING, YEAHHH!</Text>
       <StatusBar style="auto" />
     </View>
   );
