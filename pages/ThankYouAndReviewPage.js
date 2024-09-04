f3f9b3026f91cb25ec00e76820fb27fb7dde7ce1
zahara120 <zaharaa120@gmail.com>
2024-09-04 17:34:37 +0700

    feat: add react native rating

--- pages/ThankYouAndReviewPage.js
+++ pages/ThankYouAndReviewPage.js
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native";
 import { globalStyles } from "../styles/global";
 import LottieView from "lottie-react-native";
+import { Rating, AirbnbRating } from "react-native-ratings";
 export default function ThankYouAndReviewPage() {
   return (
     <View style={globalStyles.container}>
@@ -9,15 +10,16 @@ export default function ThankYouAndReviewPage() {
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
+          gap: 10,
         }}
       >
         <LottieView
-          style={{ width: 200, height: 200 }}
+          style={{ width: 150, height: 150 }}
           source={require("../assets/success.json")}
           autoPlay
           loop
         />
-        <Text>Thank you for your review</Text>
+        <AirbnbRating count={5} defaultRating={5} size={25} />
       </View>
     </View>
   );
