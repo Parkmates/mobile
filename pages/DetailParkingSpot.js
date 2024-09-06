c9b8e165d849e9a4a4bf84cbf882503bce3f0d4a
Vincent <vincentcan909@gmail.com>
2024-09-06 17:07:21 +0700

    fix: fix detail parking

--- pages/DetailParkingSpot.js
+++ pages/DetailParkingSpot.js
@@ -10,123 +10,232 @@ import {
 } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import ListReview from "../components/ListReview";
+import Header from "../components/Header";
+import { useState } from "react";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import ChooseParkingSpot from "../components/ChooseParkingSpot";
 
 export default function DetailParkingSpot({ navigation }) {
   const { width } = Dimensions.get("window");
+  const [isOpen, setIsOpen] = useState(false);
+  const [vehicle, setVehicle] = useState("");
+  const [spotDetail, setSpotDetail] = useState("")
+
+  const dummy = [1, 2, 3, 4, 5];
   return (
-    <View style={{ flex: 1, gap: 20 }}>
-      <Image
-        style={{ width: width, height: 200 }}
-        source={{
-          uri: "https://fastly.picsum.photos/id/1032/200/300.jpg?hmac=QdMNx6kwGjGtQqK_jCFOZa06MImU1ePTGi3mpwLZmwo",
-        }}
-      />
-      <View style={{ paddingHorizontal: 20, gap: 10 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-          Parking spot name
-        </Text>
-        <Text style={{ fontSize: 12 }}>
-          Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby.
-          Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240.
-        </Text>
-        <Text style={{ fontSize: 12 }}>⭐️⭐️⭐️⭐️⭐️</Text>
-      </View>
-      <View style={{ paddingHorizontal: 20, gap: 10 }}>
+    <View style={{ flex: 1, justifyContent: "space-between" , backgroundColor: '#fff'}}>
+      <Header title={"Central Park"} onPress={() => navigation.goBack()}/>
+      <ScrollView
+        style={{ flex: 1, gap: 20 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Image
+          style={{ width: width, height: 200, paddingTop: 0, marginTop: 0 }}
+          resizeMode="cover"
+          source={{
+            uri: "https://fastly.picsum.photos/id/1032/200/300.jpg?hmac=QdMNx6kwGjGtQqK_jCFOZa06MImU1ePTGi3mpwLZmwo",
+          }}
+        />
+        <View style={{ paddingHorizontal: 24, gap: 10 }}>
+          <Text style={{ fontWeight: "bold", fontSize: 20, paddingTop: 20 }}>
+            Parking spot name
+          </Text>
+          <Text style={{ fontSize: 12 }}>
+            Jl. Sultan Iskandar Muda No.7, RT.5/RW.9, Kby. Lama Sel., Kec. Kby.
+            Lama, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12240.
+          </Text>
+          <TouchableOpacity
+            style={{ flexDirection: "row", alignItems: "center", gap: 4 }}
+            onPress={() => setIsOpen(true)}
+          >
+            <Ionicons name="star" size={18} color="black" />
+            <Text style={{ fontSize: 16, textDecorationLine: "underline" }}>
+              {/* ⭐️⭐️⭐️⭐️⭐️ */}5
+            </Text>
+            <Ionicons name="chevron-forward" size={14} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View style={{ paddingHorizontal: 24, gap: 10, paddingTop: 24 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: "600",
+            }}
+          >
+            Choose Your Vehicle
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              gap: 10,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                width: "50%",
+                padding: 10,
+                backgroundColor: vehicle === "car" ? "#007BFF" : "#D9D9D9",
+                borderRadius: 10,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => setVehicle("car")}
+            >
+              <Ionicons
+                name="car"
+                size={40}
+                color={vehicle === "car" ? "#fff" : "black"}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                width: "50%",
+                padding: 10,
+                backgroundColor: vehicle === "motor" ? "#007BFF" : "#D9D9D9",
+                borderRadius: 10,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => setVehicle("motor")}
+            >
+              <Fontisto
+                name="motorcycle"
+                size={30}
+                color={vehicle === "motor" ? "#fff" : "black"}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={{ gap: 12 }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "600",
+              }}
+            >
+              Chose your parking spot
+            </Text>
+            {dummy.map((e) => {
+              return <ChooseParkingSpot 
+                key={e}  
+                floor={1}
+                area={"A1"}
+                fee={"10.000"}
+                spot={12}
+                type={spotDetail === e ? 'active' : ''}
+                onPress={() => setSpotDetail(e)}
+                vehicle={"motor"}
+              />;
+            })}
+          </View>
+          <View style={{ height: 80 }} />
+        </View>
+      </ScrollView>
+      {/* Button Book Now */}
+      {spotDetail && (
         <View
           style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            gap: 10,
+            position: "absolute",
+            bottom: 24,
+            right: 24,
+            left: 24,
           }}
         >
-          <View
+          <TouchableOpacity
             style={{
-              width: "50%",
-              padding: 10,
-              backgroundColor: "#D9D9D9",
+              width: "100%",
+              backgroundColor: "#007BFF",
               borderRadius: 10,
-              flexDirection: "row",
-              justifyContent: "center",
+              height: 50,
               alignItems: "center",
+              justifyContent: "center",
+            }}
+            onPress={() => {
+              navigation.goBack();
+              navigation.navigate("ConfirmationBooking");
             }}
           >
-            <Ionicons name="car" size={40} />
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 20,
-                paddingHorizontal: 20,
-                paddingVertical: 10,
-                textAlign: "center",
-              }}
-            >
-              5
-            </Text>
-          </View>
-          <View
-            style={{
-              width: "50%",
-              padding: 10,
-              backgroundColor: "#D9D9D9",
-              borderRadius: 10,
-              flexDirection: "row",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Ionicons name="car" size={40} />
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 20,
-                paddingHorizontal: 20,
-                paddingVertical: 10,
-                textAlign: "center",
-              }}
-            >
-              4
-            </Text>
-          </View>
-        </View>
-        <TouchableOpacity
-          style={{
-            width: "100%",
-            backgroundColor: "#007BFF",
-            borderRadius: 10,
-            height: 50,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          onPress={() => {
-            navigation.goBack();
-            navigation.navigate("ConfirmationBooking");
-          }}
-        >
-          <Text style={{ color: "white" }}>
-            Book Now
-            {/* {loading ? (
+            <Text style={{ color: "white" }}>
+              Book Now
+              {/* {loading ? (
               <ActivityIndicator size="small" color="#101010" />
             ) : (
               Book Now
             )} */}
-          </Text>
-        </TouchableOpacity>
-      </View>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
 
       {/* REVIEW */}
-      <Text style={{ fontWeight: "bold", paddingHorizontal: 20 }}>REVIEW</Text>
-      <ScrollView>
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-        <ListReview />
-      </ScrollView>
+
+      {isOpen && (
+        <BottomSheet
+          snapPoints={["80%"]}
+          enablePanDownToClose={true}
+          onClose={() => setIsOpen(false)}
+          backgroundStyle={{ backgroundColor: "#e2e2e2" }}
+          style={{ elevation: 10 }}
+        >
+          <BottomSheetScrollView
+            showsVerticalScrollIndicator={false}
+            style={{ backgroundColor: "#fff" }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                paddingHorizontal: 20,
+                textAlign: "center",
+                fontSize: 18,
+              }}
+            >
+              REVIEW
+            </Text>
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <ListReview />
+            <Text
+              style={{
+                paddingVertical: 20,
+                textAlign: "center",
+                fontSize: 14,
+              }}
+            >
+              Yay! you have seen it all
+            </Text>
+          </BottomSheetScrollView>
+        </BottomSheet>
+      )}
     </View>
   );
 }
